1d8172fcd828fcdb02818372e89d92a4bf452ece
Mintah <yawmintah1@gmail.com>
2025-01-11 17:08:35 +0000

    fixed checker error on datetime import

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -1,12 +1,12 @@
-import datetime
+from datetime import datetime, timedelta
 
 def display_current_datetime():
-    current_date = datetime.datetime.now()
-    return "Current date and time: "+ current_date.strftime("%Y-%m-%d %H:%M:%S")
+    current_date = datetime.now()
+    return "Current date and time: " + current_date.strftime("%Y-%m-%d %H:%M:%S")
 
 def calculate_future_date(days):
-    current_date = datetime.datetime.now()
-    future_date = current_date + datetime.timedelta(days)
+    current_date = datetime.now()
+    future_date = current_date + timedelta(days=days)
     return "Future date: " + future_date.strftime("%Y-%m-%d")
 
 def main():
