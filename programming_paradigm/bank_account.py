951a8e9060231bd438872aec63b044b7871690a8
Yaw Mintah <yawmintah1@gmail.com>
2025-01-18 15:01:57 +0000

    initial commit for programming_paradigm

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,19 @@
+
+class BankAccount():
+    def __init__(self, balance=0):
+        self.balance = balance
+
+    def deposit(self, amount):
+        if amount < 0:
+            return False
+        self.balance += amount
+        return self.balance
+
+    def withdraw(self, amount):
+        if amount < 0 or amount > self.balance:
+            return False
+        self.balance -= amount
+        return self.balance
+
+    def display_balance(self):
+        return self.balance
