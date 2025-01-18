b6fe1cad88e49cc31743c023cbebbeb2e5a2c3af
Yaw Mintah <yawmintah1@gmail.com>
2025-01-18 17:08:38 +0000

    fixed checker error for Methods inside the Book Class * Check out * Return Book

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -13,18 +13,18 @@ class Library:
         self._books.append(book)
     
     def check_out_book(self, title):
-        book = self.find_book(title)
+        book = next((book for book in self._books if book.title == title), None)
         if book and not book._Book__is_checked_out:
             book._Book__is_checked_out = True
             return f"{title} has been checked out."
         return f"{title} is not available for checkout."
     
-    def return_book(self, title):
-        book = self.find_book(title)
-        if book and book._Book__is_checked_out:
-            book._Book__is_checked_out = False
-            return f"{title} has been returned."
-        return f"{title} was not checked out."
+    def return_book(self):
+        book = next((book for book in self._books if book._is_checked_out), None)
+        if book:
+            book._is_checked_out = False
+            return f"{book.title} has been returned."
+        return "No books are currently checked out."
     
     def list_available_books(self):
         available_books = [book.title for book in self._books if not book._Book__is_checked_out]
